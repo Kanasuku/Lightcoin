a823dbe2e26fc5ba2339459f90ce7a3197e162b6
Shizudeo <shimizudeo@gmail.com>
2014-05-17 16:41:17 -0500

    MerkleHashRoot
    Merkle

--- src/main.cpp
+++ src/main.cpp
@@ -2806,7 +2806,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
+        assert(block.hashMerkleRoot == uint256("01ff61ff27676d3c97d28b2519d2611a5a740a6113e6cd7a835389df434ad83d"));
         block.print();
         assert(hash == hashGenesisBlock);
 
